02022823d3218fcb551aa65d83ce7457082b3b64
gilmario-kpslow <gilmariosoftware@gmail.com>
2017-08-08 23:53:09 -0300

    Primeiros Passos

--- src/main/java/br/gov/ce/caucaia/mensagerapi/EndpointWebservice.java
+++ src/main/java/br/gov/ce/caucaia/mensagerapi/EndpointWebservice.java
@@ -1,9 +1,15 @@
 package br.gov.ce.caucaia.mensagerapi;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Singleton;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -13,19 +19,36 @@ import javax.websocket.server.ServerEndpoint;
  *
  * @author gilmario
  */
-@ServerEndpoint(value = "/mensagem")
+@ServerEndpoint(value = "/chat")
+@Singleton
 public class EndpointWebservice implements Serializable {
+
+    private final Map<Session, String> sessions = new HashMap<>();
 
     @OnMessage
     public void onMensagem(Session session, String mensagem) {
-        session.getOpenSessions().forEach((s) -> {
-            s.getAsyncRemote().sendText(mensagem + " - " + LocalDateTime.now().getSecond());
+        sessions.forEach((s, id) -> {
+            try {
+                s.getBasicRemote().sendText(mensagem);
+                System.out.println("Enviando mensagem");
+            } catch (IOException ex) {
+                Logger.getLogger(EndpointWebservice.class.getName()).log(Level.SEVERE, null, ex);
+            }
         });
     }
 
     @OnOpen
-    public void onOpenSession(Session session, String mensagem) {
-        System.out.println("Conectou: " + mensagem);
+    public void onOpenSession(Session session) {
+        sessions.put(session, session.getId());
+        session.getOpenSessions().forEach((s) -> {
+            try {
+                s.getBasicRemote().sendText("Conectado " + " - " + LocalDateTime.now().getSecond());
+                System.out.println("OK");
+            } catch (IOException ex) {
+                Logger.getLogger(EndpointWebservice.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        System.out.println(sessions.size());
     }
 
     public void onClose(Session session) {
